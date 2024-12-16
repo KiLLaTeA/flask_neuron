4db271181f9d710632add6894db5fd54c3e26c91
Maxim Kostin <poster173524@mail.ru>
2024-12-16 22:24:32 +0300

    Сделал 21 лаб

--- flsite.py
+++ flsite.py
@@ -1,5 +1,10 @@
 import os
 
+import torch
+import cv2
+
+from PIL import Image
+
 import numpy as np
 import tensorflow as tf
 from flask import Flask, render_template, request, jsonify
@@ -25,10 +30,20 @@ fashion_classes = {
 }
 
 UPLOAD_FOLDER = 'model/clothes'
+DETECT_FOLDER = './static/images/Lab21/detect'
+ORIGINAL_FOLDER = './static/images/Lab21/original_images'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['DETECT_FOLDER'] = DETECT_FOLDER
+app.config['ORIGINAL_FOLDER'] = ORIGINAL_FOLDER
 
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
+
+if not os.path.exists(DETECT_FOLDER):
+    os.makedirs(DETECT_FOLDER)
+
+if not os.path.exists(ORIGINAL_FOLDER):
+    os.makedirs(ORIGINAL_FOLDER)
 
 new_neuron = SingleNeuron(input_size=3)
 new_neuron.load_weights('model/neuron_weights.txt')
@@ -36,6 +51,9 @@ model_reg = tf.keras.models.load_model('model/Lab16/regression_model.h5')
 model_class = tf.keras.models.load_model('model/Lab16/classification_model.h5')
 model_fashion = tf.keras.models.load_model('model/Lab17/fashion.h5')
 model_fashion_CNN = tf.keras.models.load_model('model/Lab18/fashion_CNN.h5')
+model_YOLO = torch.hub.load(repo_or_dir='./model/Lab21/yolov5', model='yolov5s', source = 'local')
+# model_YOLO = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+
 
 @app.route("/")
 def index():
@@ -160,7 +178,78 @@ def upload_CNN():
 
 @app.route("/Lab_21", methods=['POST', 'GET'])
 def upload_21():
-    pass
+    if request.method == 'GET':
+        return render_template('detection.html', title="Детектирование объектов", menu=menu, detected_images='')
+    if request.method == 'POST':
+        if 'image' not in request.files:
+            return "Нету пути файла"
+
+        file = request.files['image']
+
+        if file.filename == '':
+            return "Не выбран файл"
+
+        if file:
+            img_path = os.path.join(app.config['ORIGINAL_FOLDER'], file.filename)
+            file.save(img_path)
+
+            results = model_YOLO(img_path)
+            results.save(labels=True, save_dir=os.path.join(app.config['DETECT_FOLDER']), exist_ok=True)
+
+            desired_classes = ['person']
+            confidence_threshold = 0.75
+
+            filtered_results = results.pandas().xyxy[0]
+            filtered_results = filtered_results[(filtered_results['name'].isin(desired_classes)) & (
+                        filtered_results['confidence'] >= confidence_threshold)]
+
+            img = Image.open(os.path.join(app.config['ORIGINAL_FOLDER'], file.filename))
+
+            filtered_image = np.array(img)
+            filtered_image = cv2.cvtColor(filtered_image, cv2.COLOR_RGB2BGR)
+
+            for _, row in filtered_results.iterrows():
+                label = row['name']
+                conf = row['confidence']
+                xmin, ymin, xmax, ymax = row[['xmin', 'ymin', 'xmax', 'ymax']]
+                cv2.rectangle(filtered_image, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (255, 0, 0), 2)
+                cv2.rectangle(filtered_image, (int(xmin), int(ymin) - 20), (int(xmax), int(ymin)), (255, 0, 0), -1)
+                cv2.putText(filtered_image, f'{label} {conf:.2f}', (int(xmin), int(ymin)), cv2.FONT_HERSHEY_SIMPLEX,
+                            0.8, (255, 255, 255), 1)
+
+            cv2.imwrite(os.path.join(app.config['DETECT_FOLDER'], '2_detected_'+file.filename), filtered_image)
+
+            # Фильтрация результатов по классам и вероятности
+            filtered_results = []
+            for *box, conf, cls in results.xyxy[0]:
+                label = model_YOLO.names[int(cls)]
+                if label in desired_classes and conf >= confidence_threshold:
+                    filtered_results.append((box, conf, cls))
+            image_np = np.array(img)
+
+            image_np = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
+
+            paths = [
+                "/static/images/Lab21/original_images/" + file.filename,
+                "/static/images/Lab21/detect/" + file.filename,
+                "/static/images/Lab21/detect/" + '2_detected_'+file.filename,
+            ]
+
+            cropped_images = []
+            count = 0
+            for box, conf, cls in filtered_results:
+                count += 1
+                xmin, ymin, xmax, ymax = map(int, box)
+                cropped_image = image_np[ymin:ymax, xmin:xmax]
+                cropped_images.append(cropped_image)
+                path_classes = os.path.join(app.config['DETECT_FOLDER'], '3_class'+ str(count) +'_' + file.filename)
+                cv2.imwrite(path_classes, cropped_image)
+                paths.append("/static/images/Lab21/detect/" + '3_class'+ str(count) +'_' + file.filename)
+
+            print(paths)
+
+            return render_template('detection.html', title="Детектирование объектов", menu=menu,
+                                   detected_images=paths)
 
 
 if __name__ == "__main__":
